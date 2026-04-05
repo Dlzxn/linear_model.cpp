97b0dd6a5ea8dc5c55ff11e84e5df5770a34f377
Dlzxn <aleshamarichev09@gmail.com>
2026-04-05 17:53:46 +0300

    add MAE

--- loss_func/loss_function.cpp
+++ loss_func/loss_function.cpp
@@ -4,3 +4,12 @@
 float MSE(float predict, float true_value){
         return (predict-true_value) * (predict-true_value);
     }
+
+float MAE(float predict, float true_value){
+        if ((predict-true_value) > 0) {
+            return (predict-true_value);
+        }
+        else {
+            return -(predict-true_value);
+        }
+    }
