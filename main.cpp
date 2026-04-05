bfb006cc04c513ea4c84b4ef48efbcaaa632ab0d
Dlzxn <aleshamarichev09@gmail.com>
2026-04-05 17:34:52 +0300

    main.cpp impl linear model

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include "loss.h"
 #include "Linear_model.h"
+#include "optimizer.h"
 
 using namespace std;
 
 
+int EPOCH = 5;
+float lr = 0.01;
+
+
 int main(){
-    cout<<MSE(4, 2);
-    LinearModel model = LinearModel(15);
+    LinearModel model = LinearModel(3);
+    Optimizer optim = Optimizer(lr, model.get_weights());
+
+    vector<float> data = {2.0, 3.0, 4.0};
+    for (int i = 0; i < EPOCH; i++) {
+        float predict = model.predict(data);
+        cout<<predict;
+        float loss = MSE(predict, 9.00);
+        optim.step(loss, model.backprop());
+    }
+    cout<<"Param:";
+    for (int i=0; i< model.get_weights().size(); i++) {
+        cout<<model.get_weights()[i];
+    }
     return 0;
 }
